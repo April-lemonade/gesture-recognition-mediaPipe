50746aa7ad656cffe01b8c3f4b64929863e1df21
April-lemonade <641676557@qq.com>
2024-03-16 16:57:34 +0800

    手势图片逐渐消失，与视频对齐

--- script1.js
+++ script1.js
@@ -23,7 +23,8 @@ let webcamRunning = false;
 const videoHeight = "360px";
 const videoWidth = "480px";
 let lastResult = "";
-let lastResults = []
+let lastResults = [];
+let gestures = ["wan1"];
 // Before we can use HandLandmarker class we must wait for it to finish
 // loading. Machine Learning models can be large and take a moment to
 // get everything needed to run.
@@ -45,81 +46,11 @@ const createGestureRecognizer = async () => {
     demosSection.classList.remove("invisible");
 };
 createGestureRecognizer();
+let index = Math.random() * (gestures.length - 1);
+let currentGesture = gestures[index];
+const gestureImg = document.getElementById("gestureImg");
+gestureImg.src = "/img/" + currentGesture + ".png";
 
-
-/********************************************************************
- // Demo 1: Detect hand gestures in images
- ********************************************************************/
-const imageContainers = document.getElementsByClassName("detectOnClick");
-for (let i = 0; i < imageContainers.length; i++) {
-    imageContainers[i].children[0].addEventListener("click", handleClick);
-}
-
-async function handleClick(event) {
-    if (!gestureRecognizer) {
-        alert("Please wait for gestureRecognizer to load");
-        return;
-    }
-    if (runningMode === "VIDEO") {
-        runningMode = "IMAGE";
-        await gestureRecognizer.setOptions({runningMode: "IMAGE"});
-    }
-    // Remove all previous landmarks
-    const allCanvas = event.target.parentNode.getElementsByClassName("canvas");
-    for (var i = allCanvas.length - 1; i >= 0; i--) {
-        const n = allCanvas[i];
-        n.parentNode.removeChild(n);
-    }
-    const results = gestureRecognizer.recognize(event.target);
-    // View results in the console to see their format
-    console.log(results);
-    if (results.gestures.length > 0) {
-        const p = event.target.parentNode.childNodes[3];
-        p.setAttribute("class", "info");
-        const categoryName = results.gestures[0][0].categoryName;
-        const categoryScore = parseFloat(results.gestures[0][0].score * 100).toFixed(2);
-        const handedness = results.handednesses[0][0].displayName;
-        p.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore}%\n Handedness: ${handedness}`;
-        p.style =
-            "left: 0px;" +
-            "top: " +
-            event.target.height +
-            "px; " +
-            "width: " +
-            (event.target.width - 10) +
-            "px;";
-        const canvas = document.createElement("canvas");
-        canvas.setAttribute("class", "canvas");
-        canvas.setAttribute("width", event.target.naturalWidth + "px");
-        canvas.setAttribute("height", event.target.naturalHeight + "px");
-        canvas.style =
-            "left: 0px;" +
-            "top: 0px;" +
-            "width: " +
-            event.target.width +
-            "px;" +
-            "height: " +
-            event.target.height +
-            "px;";
-        event.target.parentNode.appendChild(canvas);
-        const canvasCtx = canvas.getContext("2d");
-        const drawingUtils = new DrawingUtils(canvasCtx);
-        for (const landmarks of results.landmarks) {
-            drawingUtils.drawConnectors(landmarks, GestureRecognizer.HAND_CONNECTIONS, {
-                color: "#00FF00",
-                lineWidth: 5
-            });
-            drawingUtils.drawLandmarks(landmarks, {
-                color: "#FF0000",
-                lineWidth: 1
-            });
-        }
-    }
-}
-
-/********************************************************************
- // Demo 2: Continuously grab image from webcam stream and detect it.
- ********************************************************************/
 const video = document.getElementById("webcam");
 const canvasElement = document.getElementById("output_canvas");
 const canvasCtx = canvasElement.getContext("2d");
@@ -151,10 +82,14 @@ function enableCam(event) {
     if (webcamRunning === true) {
         webcamRunning = false;
         enableWebcamButton.style.display = "none";
+        // gestureImg.style.display = "block";
+        gestureImg.style.opacity = "1";
     } else {
         webcamRunning = true;
         // enableWebcamButton.innerText = "DISABLE PREDICTIONS";
         enableWebcamButton.style.display = "none";
+        // gestureImg.style.display = "block";
+        gestureImg.style.opacity = "1";
     }
     // get Usermedia parameters.
     const constraints = {
@@ -238,18 +173,26 @@ async function predictWebcam() {
                 video1.style.display = "block";
                 video1.src = "/video/wan.mp4";
                 enableWebcamButton.style.display = "none";
+                // gestureImg.style.display = "none";
+                gestureImg.style.opacity = "1";
                 video1.addEventListener("ended", function () {
                     video1.style.display = "none";
+                    // gestureImg.style.display = "block";
+                    gestureImg.style.opacity = "1";
                     video1.src = "";
                     // enableWebcamButton.style.display = "block";
                     lastResult = "";
                     lastResults = [];
                 })
-                lastResults = []
+                // lastResults = []
             } else if (lastResults[lastResults.length - 1] === categoryName || lastResults.length === 0) {
-                lastResults.push(categoryName)
+                lastResults.push(categoryName);
+                // gestureImg.style.display = "none";
+                gestureImg.style.opacity = "0";
             } else if (lastResults[lastResults.length - 1] !== categoryName) {
-                lastResults = []
+                lastResults = [];
+                // gestureImg.style.display = "block";
+                gestureImg.style.opacity = "1";
             }
             // lastResult = categoryName
             // lastResults.append(categoryName)
